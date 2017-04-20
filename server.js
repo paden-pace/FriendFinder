e257f4f2f24575d6b86b0123128383335e9adbd9
Paden Pace <pjpace27@yahoo.com>
2017-04-19 23:48:49 -0500

    addition of comparison function

--- server.js
+++ server.js
@@ -26,7 +26,17 @@ app.use(bodyParser.json({
   type: "application/vnd.api+json"
 }));
 
-var members = [];
+var members = [
+    {
+    name: 'Bob',
+    photo: 'Bob.com',
+    answers: [ '5', '5', '5', '5', '5', '5', '5', '5', '5', '5' ]
+    },{
+    name: 'John',
+    photo: 'John.com',
+    answers: [ '5', '5', '5', '5', '5', '1', '1', '1', '1', '1' ]
+    },
+];
 
 // app.get("./routing/htmlRoutes.js", homeRoute(req, res));
     app.get("/", function(req, res){
@@ -38,7 +48,7 @@ var members = [];
     });
 
 
-app.get("/api/:members?", function(req, res) {
+app.get("/api", function(req, res) {
   var chosen = req.params.members;
 
   if (chosen) {
@@ -59,11 +69,37 @@ app.get("/api/:members?", function(req, res) {
 app.post("/api/new", function(req, res) {
   // req.body hosts is equal to the JSON post sent from the user
     var newMember = req.body;
-    newMember.routeName = newMember.name.replace(/\s+/g, "").toLowerCase();
     console.log(newMember);
+
+    var newMemberArray = newMember.answers;
+    var least = 0;
+    for (i=0; i<members.length; i++){
+        var results = 0;
+        var leastResults = 40;
+            for (k=0; k<members[i].answers.length; k++){
+                var check = parseInt(members[i].answers) - parseInt(newMember.answers[k]);
+                console.log(check);
+                if (check > 0){
+                    results = results + check;
+                } else if (check < 0){
+                    results = results - check;
+                };
+            };
+        console.log("Member "+ i + " has a result of " + results);
+        if (results < 40){
+            if (results < leastResults){
+                least = i;
+            }
+        } 
+        console.log ("least: " + least);
+    };
+    return res.json(members[least]);
+    // $("#bestFriendDiv").append("<h2> Friend Name: " + members[least].name + "</h2>");
+    // $("#bestFriendDiv").append("<h2> Friend Picture: " + members[least].photo + "</h2>");
+// compare answer, add to total number, then use total number to find the smallest, that is the one
+
   // We then add the json the user sent to the member array
     members.push(newMember);
-    newMember.conirmation = 'confirm member';
   // We then display the JSON to the users
     res.json(newMember);
 });
